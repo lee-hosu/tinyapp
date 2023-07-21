ec4200f7cac4ffdc0856a4ddcd6655f36b4e4d16
Hosu <sogeumhosu@gmail.com>
2023-07-21 11:26:18 -0700

    Fix generateRandomString function to return string instead of number

--- express_server.js
+++ express_server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
-function generateRandomString() {
-  const min = 100000;
-  const max = 999999;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
+  let randomString = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
+const randomString = generateRandomString(6);
 
 // CONFIG for rendering EJS templates
 app.set('view engine', 'ejs');
